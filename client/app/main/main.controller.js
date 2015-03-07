d39167d992c40cd2d2c743338b7b2157f181ee7a
Tim Gallant <me@timgallant.us>
2015-03-07 13:05:34 -0700

    adding d3 gmaps

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,31 +3,89 @@
 angular.module('angularTpdVisApp')
 .controller('MainCtrl', function ($scope, $http, socket, uiGmapGoogleMapApi) {
 
-  $scope.map = { center: { latitude: 32.2136761, longitude: -110.8237743 }, zoom: 12 };
-
-  $scope.mapStyles = [
-  {
-    stylers: [
-    { hue: "#00ffe6" },
-    { saturation: -20 }
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-    { lightness: 100 },
-    { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-    { visibility: "off" }
-    ]
-  }
-  ];
-
   uiGmapGoogleMapApi.then(function(maps) {
-    console.log('works');
-  }); 
+    var makeOverlay = function(map) {
+      d3.json("/api/incidents", function(data) {
+
+        var data = [{latitude: 32.2317,longitude: -110.9519}];
+
+        var overlay = new maps.OverlayView();
+
+        overlay.onAdd = function() {
+          var layer = d3.select(this.getPanes().overlayLayer).append("div")
+          .attr("class", "stations");
+
+          overlay.draw = function() {
+            var projection = this.getProjection(),
+            padding = 10;
+
+            var marker = layer.selectAll("svg")
+            .data(data)
+            .each(transform)
+            .enter().append("svg:svg")
+            .each(transform)
+            .attr("class", "marker");
+
+            marker.append("svg:circle")
+            .attr("r", 300)
+            .attr("cx", padding)
+            .attr("cy", padding);
+
+            marker.append("svg:text")
+            .attr("x", padding + 7)
+            .attr("y", padding)
+            .attr("dy", ".31em")
+            .text(function(d) { return d.key; });
+
+            function transform(d) {
+              console.log(d);
+              d = new maps.LatLng(d.latitude, d.longitude);
+              d = projection.fromLatLngToDivPixel(d);
+              console.log(d);
+              return d3.select(this)
+              .style("left", (d.x - padding) + "px")
+              .style("top", (d.y - padding) + "px");
+            };
+          };
+        };
+        overlay.setMap(map);
+      });
+};
+
+$scope.map = 
+{ 
+  center: { 
+    latitude: 32.2317,
+    longitude: -110.9519
+  },
+  zoom: 12,
+  events: {
+    tilesloaded: function (map) {
+      makeOverlay(map);
+    }
+  }
+};
+
+$scope.mapStyles = [
+{
+  stylers: [
+  { hue: "#00ffe6" },
+  { saturation: -20 }
+  ]
+},{
+  featureType: "road",
+  elementType: "geometry",
+  stylers: [
+  { lightness: 100 },
+  { visibility: "simplified" }
+  ]
+},{
+  featureType: "road",
+  elementType: "labels",
+  stylers: [
+  { visibility: "off" }
+  ]
+}
+];
+});
 });
