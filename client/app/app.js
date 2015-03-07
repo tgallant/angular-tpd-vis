2ef90aeede6a8fd59dcb10ee6d4ffd6ed2f26de8
Tim Gallant <me@timgallant.us>
2015-03-07 00:59:18 -0700

    adding

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('angularTpdVisApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'uiGmapgoogle-maps'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('angularTpdVisApp')
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider, uiGmapGoogleMapApiProvider) {
+  	uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyCFT_EeToQJZ2ZazF5RjkoJIzhL_QRQhqU',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+
     $stateProvider
       .state('main', {
         url: '/',
