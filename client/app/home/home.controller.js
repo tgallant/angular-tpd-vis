897e0aaa196cf36bc4493d5770ea69fe879f98c5
Tim Gallant <me@timgallant.us>
2015-03-08 08:47:13 -0700

    merging

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,12 +1,8 @@
 'use strict';
 
 angular.module('angularTpdVisApp')
-<<<<<<< HEAD
-.controller('HomeCtrl', function ($scope) {
-=======
   .controller('HomeCtrl', function ($scope, $rootScope) {
 
     $rootScope.date = "";
->>>>>>> 037835be48775854447532f06d67b58527d1c884
     $scope.message = 'Hello';
 });
