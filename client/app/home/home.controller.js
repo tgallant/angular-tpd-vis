12af7d953f8dd0c5a1f3506dffc21387f9d77983
Tim Gallant <me@timgallant.us>
2015-03-08 08:40:47 -0700

    homepage stuff

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,17 +1,6 @@
 'use strict';
 
 angular.module('angularTpdVisApp')
-  .controller('HomeCtrl', function ($scope) {
+.controller('HomeCtrl', function ($scope) {
     $scope.message = 'Hello';
-    $scope.awesomeThings = [
-    {
-    name: 'Source', 
-    link:'http://data.tucsonaz.gov',
-    info:'A big thanks to the City of Tucson.'
-    },
-    {
-    name: 'D3',
-    link: 'http://d3js.org',
-    },
-    ];
-  });
+});
