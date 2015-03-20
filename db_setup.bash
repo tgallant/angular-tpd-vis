04063e668fd8ef57637d4ec96dbe0280c091dc96
Sean Stephens <seanastephens@email.arizona.edu>
2015-03-19 22:59:53 -0700

    Added checks for python/dos2unix dependencies.

--- db_setup.bash
+++ db_setup.bash
@@ -15,10 +15,12 @@ mongo angulartpdvis-dev --eval "db.incidents.drop()"
 mongoimport --port 27017 --db angulartpdvis-dev --type csv --file setup/incidents.csv --collection incidents --fieldFile setup/header.txt
 
 # Convert date fields with strings to Date objects.
-mongo angulartpdvis-dev --eval "db.incidents.find().forEach(function(doc) { 
-     doc.DATE_OCCU=new Date(doc.DATE_OCCU);
-     doc.DATE_REPT=new Date(doc.DATE_REPT);
-     doc.DATE_FND=new Date(doc.DATE_FND);
-     doc.ACTDATE=new Date(doc.ACTDATE);
-     db.incidents.save(doc); 
-     })"
+mongo angulartpdvis-dev --eval "
+    db.incidents.find().forEach(function(doc) { 
+        doc.DATE_OCCU=new Date(doc.DATE_OCCU);
+        doc.DATE_REPT=new Date(doc.DATE_REPT);
+        doc.DATE_FND=new Date(doc.DATE_FND);
+        doc.ACTDATE=new Date(doc.ACTDATE);
+        db.incidents.save(doc); 
+    });
+"
--- setup/pull_data.bash
+++ setup/pull_data.bash
@@ -1,5 +1,20 @@
 LOG=logs/$(date +%h-%d-%y_%H:%M).incidents.log
 
+#Check dependencies.
+if ! which python > /dev/null ; then
+    echo "Need an installation of python. Try"
+    echo "sudo apt-get install python"
+    exit 1
+elif ! python -c "import pyproj" >& /dev/null ; then
+    echo "Need to install pyproj. Try"
+    echo "sudo pip install pyproj"
+    exit 1
+elif ! which dos2unix >/dev/null ; then
+    echo "Need an installation of dos2unix. Try"
+    echo "sudo apt-get install dos2unix"
+    exit 1
+fi
+
 if ! wget http://www.tucsonaz.gov/files/transportation/files/TPD_Incidents_45Day.zip ; then
     echo "Could not fetch data file; maybe the URL changed or the connection is bad."
     echo "exiting."
