e498857c3caf9d06a61b7c3ba78394b8d2d1bcf4
Sean Stephens <seanastephens@email.arizona.edu>
2015-03-27 21:16:21 -0700

    Removed api/things

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -20,7 +20,6 @@ function onConnect(socket) {
   // Insert sockets below
   require('../api/mindate/mindate.socket').register(socket);
   require('../api/incident/incident.socket').register(socket);
-  require('../api/thing/thing.socket').register(socket);
 }
 
 module.exports = function (socketio) {
